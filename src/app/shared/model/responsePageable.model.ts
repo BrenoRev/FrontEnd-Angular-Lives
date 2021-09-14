80be2ef02e1ca435d51d58c8f9f69b538a87315a
BrenoRev <silvabreno462@gmail.com>
2021-09-14 18:12:33 -0300

    Criando a classe model que sera recebida do back-end

---
+++ src/app/shared/model/responsePageable.model.ts
@@ -0,0 +1,13 @@
+export class ResponsePageable{
+
+    content!: any[];
+    first!: boolean;
+    last!: boolean;
+    number!: number;
+    numberOfElements!: number;
+    pageable!: any[];
+    size!: number;
+    sort!: number;
+    totalElements!: number;
+    totalPages!: number;
+}
