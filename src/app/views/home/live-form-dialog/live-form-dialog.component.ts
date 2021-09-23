db16a5bd71a004d022f61ffa0cb8bc7df971e465
BrenoRev <silvabreno462@gmail.com>
2021-09-22 23:39:37 -0300

    Fazendo o método post para o back-end

--- src/app/views/home/live-form-dialog/live-form-dialog.component.ts
+++ src/app/views/home/live-form-dialog/live-form-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
+import { LiveService } from 'src/app/shared/service/live.service';
 
 @Component({
   selector: 'app-live-form-dialog',
@@ -9,7 +10,9 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class LiveFormDialogComponent implements OnInit {
 
+  private rest: LiveService;
   public liveForm: FormGroup;
+
   constructor(public dialogRef: MatDialogRef<LiveFormDialogComponent>,
               public fb: FormBuilder) { }
 
@@ -27,10 +30,14 @@ export class LiveFormDialogComponent implements OnInit {
 
   cancelar(): void {
     this.dialogRef.close();
+    this.liveForm.reset();
   }
 
   createLive(){
-    
+    // Faz um post passando os itens do formulario
+    this.rest.postLives(this.liveForm.value).subscribe(result => {});
+    this.dialogRef.close();
+    this.liveForm.reset();
   }
 
 }
